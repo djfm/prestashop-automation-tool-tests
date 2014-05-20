52609b260da88ec005ee6a32715fc60401bad311
fram <fm.de.jouvencel@gmail.com>
2014-05-20 08:36:03 +0200

    + installation test

---
+++ install.in.all.languages.rb
@@ -0,0 +1,38 @@
+require 'prestashop-automation-tool/helper'
+
+configurations = {
+	'bn' => %w(bd fr),
+	'br' => %w(br de),
+	'ca' => %w(es fr),
+	'cs' => %w(cz fr),
+	'de' => %w(de it),
+	'en' => %w(us pt),
+	'es' => %w(es ru),
+	'fa' => %w(ir nl),
+	'fr' => %w(fr it),
+	'hu' => %w(hu de),
+	'id' => %w(id cz),
+	'it' => %w(it hu),
+	'mk' => %w(mk ru),
+	'nl' => %w(nl de),
+	'no' => %w(no ro),
+	'pl' => %w(pl ru),
+	'ru' => %w(ru fr),
+	'sr' => %w(rs it),
+	'tr' => %w(tr de),
+	'tw' => %w(tw cn),
+	'zh' => %w(cn tw)
+}
+
+
+describe 'Installation of PrestaShop' do
+	configurations.each_pair do |language, countries|
+		countries.each do |country|
+			it "should succeed with country '#{country}' and language '#{language}'" do
+				@shop.clear_cookies
+				@shop.drop_database
+				@shop.install :country => country, :language => language
+			end
+		end
+	end
+end
